70a5f0073f07457eb1e2cfd1523ef8e2a27861d4
aspunds <jonathan@asplund.net>
2020-11-09 23:43:21 +0100

    Revert "Revert "fancy slider""
    This reverts commit 870b8be7bea6d457fe80a8f1c2bc4104521380d1.

--- Företag/sida.js
+++ Företag/sida.js
@@ -1,20 +1,6 @@
-var slideIndex = 1;
 
 document.addEventListener("DOMContentLoaded", e => {
-  showDivs(slideIndex); 
+  new Splide(".splide", {
+    type: 'loop',
+  }).mount();
 });
-
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    x[slideIndex-1].style.display = "block";  
-  }
